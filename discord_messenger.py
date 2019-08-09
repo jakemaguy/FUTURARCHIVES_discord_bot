e3b76f4d771293d3a775d7fa08155e94c83cff80
Jake <jakemaguy@gmail.com>
2019-08-09 04:48:31 -0400

    Fixed Constructor Issues

--- discord_messenger.py
+++ discord_messenger.py
@@ -1,14 +1,13 @@
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
-
 class DiscordMessenger:
 
-    def __init__(self):
+    def __init__(self, webhook_url):
         self.webhook = DiscordWebhook(url=webhook_url, username="FUTUR.io")
         self.website_url = 'https://futurarchives.com/password'
     
     def send_warning_message(self, message):
-        embed = DiscordEmbed(title="Warning Message", description=message, color=9e0310)
+        embed = DiscordEmbed(title="Warning Message", description=message, color=242424)
         embed.set_timestamp()
         self.webhook.add_embed(embed)
         self.webhook.execute()
